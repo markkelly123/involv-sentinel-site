eb753124f59bd34de88ac4fe50ccd1e3877ebe0c
Mark Kelly <markkelly@Marks-MacBook-Air-2.local>
2025-09-07 21:49:36 +1000

    Fix Sanity API version error

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -3,7 +3,7 @@ import { createClient } from '@sanity/client'
 export const sanity = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
+  apiVersion: '2024-01-01',
   useCdn: true,
 })
 
